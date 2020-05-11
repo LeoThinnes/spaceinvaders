6d9197069c0a95d41cfaff2b14fdb741b7ffdd21
Léo Thinnes <leo.thinnes@etu.unilim.fr>
2020-05-11 14:52:44 +0200

    positionner un vaisseau avec une dimension

--- src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTests.java
+++ src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTests.java
@@ -4,7 +4,8 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.fail;
-import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
+
+import fr.unilim.iut.spaceinvaders.utils.*;
 
 public class SpaceInvadersTests {
 	
@@ -174,6 +175,24 @@ public class SpaceInvadersTests {
 			"...............\n" + 
 			".......VVV.....\n" + 
 			".......VVV.....\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+		}
+	    
+	    @Test
+		public void test_UnNouveauVaisseauPositionneDansEspaceJeuMaisAvecDimensionTropGrande_DoitLeverUneExceptionDeDebordement() {
+			
+			try {
+				spaceinvaders.positionnerUnNouveauVaisseau(9,2,7,9);
+				fail("Dépassement du vaisseau à droite en raison de sa longueur trop importante : devrait déclencher une exception DebordementEspaceJeuException");
+			} catch (final DebordementEspaceJeuException e) {
+			}
+			
+			
+			try {
+				spaceinvaders.positionnerUnNouveauVaisseau(3,4,7,1);
+				fail("Dépassement du vaisseau vers le haut en raison de sa hauteur trop importante : devrait déclencher une exception DebordementEspaceJeuException");
+			} catch (final DebordementEspaceJeuException e) {
+			}
+				
 		}
 }
 
